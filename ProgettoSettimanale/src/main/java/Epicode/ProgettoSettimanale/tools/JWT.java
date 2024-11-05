10a1468e52fcb12cb85146fe363a6d532d7d6915
sudonona <80674485+sudonona@users.noreply.github.com>
2024-11-05 16:20:36 +0100

    adds

--- ProgettoSettimanale/src/main/java/Epicode/ProgettoSettimanale/tools/JWT.java
+++ ProgettoSettimanale/src/main/java/Epicode/ProgettoSettimanale/tools/JWT.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import riccardogulin.u5d12.exceptions.UnauthorizedException;
 
 import java.util.Date;
 
